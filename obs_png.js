857de24c50c256dca3103092a39ab041d59da825
MartinDac <martincunha.avila@gmail.com>
2025-09-05 22:43:49 -0300

    Refactor WebSocket image handling in server.js and obs_png.js to use ES6 imports and update image source logic

--- obs_png.js
+++ obs_png.js
@@ -1,17 +1,12 @@
-require('dotenv').config();
 
 const avatar = document.getElementById("avatar");
-const idleImg = process.env.IDLE_IMG;     // boca cerrada
-const talkingImg = process.env.TALKING_IMG; // boca abierta
 
-    // Conectamos al servidor WebSocket que corre en Node.js
+// Conectamos al servidor WebSocket que corre en Node.js
 const ws = new WebSocket("ws://localhost:8080");
 
 ws.onmessage = (event) => {
-    const { volume } = JSON.parse(event.data);
-    if (volume > 0.02) { // Ajusta el umbral según tu micrófono
-    avatar.src = talkingImg;
-    } else {
-    avatar.src = idleImg;
+    const data = JSON.parse(event.data);
+    if (data.image) {
+        avatar.src = data.image;
     }
 };
--- server.js
+++ server.js
@@ -1,33 +1,47 @@
-import OBSWebSocket, {EventSubscription} from 'obs-websocket-js';
-import WebSocket from 'ws';
-import 'dotenv/config';
+import OBSWebSocket, { EventSubscription } from "obs-websocket-js";
+import WebSocket from "ws";
+import "dotenv/config";
 
 const obs = new OBSWebSocket();
 const wss = new WebSocket.Server({ port: 8080 }); // Servidor para el widget
 
+const idleImg = process.env.IDLE_IMG; // boca cerrada
+const talkingImg = process.env.TALKING_IMG; // boca abierta
+
 (async () => {
   try {
     const port = process.env.PORT || 4455;
-    const password = process.env.PASSWORD || '1234';
-    const ip = process.env.IP || 'localhost';
+    const password = process.env.PASSWORD || "1234";
+    const ip = process.env.IP || "localhost";
+
     await obs.connect(`ws://${ip}:${port}`, password, {
-        eventSubscriptions: EventSubscription.InputVolumeMeters,
-        rpcVersion: 1
+      eventSubscriptions: EventSubscription.InputVolumeMeters,
+      rpcVersion: 1,
     }); // URL y contraseña
     console.log("Conectado a OBS");
 
-    obs.on('InputVolumeMeters', data => {
-      const mic = data.inputs.find(i => i.inputName === "Mic/Aux");
+    obs.on("InputVolumeMeters", (data) => {
+      const mic = data.inputs.find((i) => i.inputName === "Mic/Aux");
+
       if (mic) {
         const volume = mic.inputLevelsMul[0];
-        wss.clients.forEach(client => {
+        let imgName;
+
+        if (volume > 0.02) {
+          // Ajusta el umbral según tu micrófono
+          console.log("Hablando", volume);
+          imgName = talkingImg;
+        } else {
+          imgName = idleImg;
+        }
+
+        wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ volume }));
+            client.send(JSON.stringify({ image: imgName }));
           }
         });
       }
     });
-
   } catch (err) {
     console.error(err);
   }
