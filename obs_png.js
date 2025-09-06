f46d7fa7591a618e6ff775b87199ad4f271a0770
Martin de Avila Cunha <martincunha.avila@gmail.com>
2025-09-05 22:46:47 -0300

    Add logging for WebSocket connection and image data

--- obs_png.js
+++ obs_png.js
@@ -3,10 +3,13 @@ const avatar = document.getElementById("avatar");
 
 // Conectamos al servidor WebSocket que corre en Node.js
 const ws = new WebSocket("ws://localhost:8080");
+console.log("Inicia");
 
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
+    console.log(data);
     if (data.image) {
+        console.log(data.image);
         avatar.src = data.image;
     }
 };
